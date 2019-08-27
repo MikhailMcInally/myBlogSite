1acc737971c486b02a47af4e5a24eb2ea0630540
Mikhail <mikhailmcinally12@gmail.com>
2019-08-26 23:21:19 -0700

    add vendor directory and gulp vendor task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const uglifycss = require('gulp-uglifycss');
 const browsersync = require('browser-sync').create();
+const merge = require('merge-stream');
+const header = require('gulp-header');
+const del = require('del');
 
 // Load package.json
 const pkg = require('./package.json');
@@ -32,12 +35,35 @@ function browserSyncReload(done) {
     done();
 }
 
+//Clean Vendor
+function clean() {
+    return del(['./vendor/']);
+}
+
+// Move dependencies from node_modules into vendor directory
+function modules() {
+    //Bootstrap
+    let bootstrap = gulp.src('./node_modules/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'));
+    // Font Awesome CSS
+    // Font Awesome Webfonts
+    // jQuery
+    let jquery = gulp.src([
+        './node_modules/jquery/dist/*',
+        '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+    return merge(bootstrap, jquery);
+}
+
+
 //Watch files
 function watchFiles() {
     gulp.watch('./css/blog.css', browserSyncReload);
 }
 
 //Define Complex Tasks
+const vendor = gulp.series(clean, modules);
 const watch = gulp.parallel(watchFiles, browserSync);
 
 
@@ -48,3 +74,5 @@ const watch = gulp.parallel(watchFiles, browserSync);
 
 
 exports.watch = watch;
+exports.clean = clean;
+exports.vendor = vendor;
